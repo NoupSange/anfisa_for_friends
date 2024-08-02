132a3a9ad2fab6165500b3062a3ede12a15a2404
Mickhail Fedoseev <fedoseevmvf@yandex.ru>
2024-08-02 16:44:07 +0300

    make database context for homepage

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
     context = {}
@@ -11,3 +10,4 @@ def ice_cream_list(request):
     template = 'ice_cream/list.html'
     context = {}
     return render(request, template, context)
+
